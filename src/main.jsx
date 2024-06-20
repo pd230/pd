aff130b5845e39ead19fa74af7f4b2b2d6c47eca
pratiksha <pratikshadixit233@gmail.com>
2024-06-20 21:47:23 +0530

    Updated my_vite with new modifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// const refApp = "chai aur RJS"
+
+// const anotherElement = (
+//       <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+//                 //  or
+
+// const reactElemet = React.createElement(
+//   'a',
+//   {href:'https://google.com', target:'_blank'},
+//   'Click to visit google     ',
+//    refApp
+// )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
+  // anotherElement
+  // reactElemet
     <App />
-  </React.StrictMode>,
+  
 )
